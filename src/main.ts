c6bb108cf76a43fc589b9118339154c0e912e184
Roman Chechyotkin <romanchechyotkin@gmail.com>
2022-10-14 21:30:26 +0300

    before redux

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-
+    app.enableCors();
     app.useGlobalPipes(new ValidationPipe());
-
     await app.listen(process.env.PORT, () => {
         console.log(`server running on port ${process.env.PORT}`)
     });
